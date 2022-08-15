af992e49e097e6a81c7e97d91b87c94c45dd8bf5
Laniel88 <ryanhb0430@gmail.com>
2022-08-15 23:49:16 +0900

    main commit before test

---
+++ gui/mainGroup.py
@@ -0,0 +1,93 @@
+from asyncio.windows_events import NULL
+from urllib import request
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class MainGroupClass():
+    re_color_list = ('rgb(249,205,173)',
+                     'rgb(200,200,169)', 'rgb(156, 158, 254)')
+
+    def loadMenuGroup(self, labelIndex, menuData, listOfMenuGroup):
+        # show menuGroup
+        listOfMenuGroup[0].show()
+
+        # settings of menuImage
+        img = request.urlopen(menuData[0]).read()
+        listOfMenuGroup[3].setPixmap(self.mask_image(img, 'jpeg'))
+
+        # set titleLabelA
+        listOfMenuGroup[1].setText(self.reLabel[labelIndex])
+        listOfMenuGroup[1].setStyleSheet(
+            'color:' + MainGroupClass.re_color_list[labelIndex])
+        listOfMenuGroup[1].setFont(
+            self.addFontWithPixel(21, 'Noto Sans KR', True))
+
+        # move titlelabelB
+        if labelIndex == 0:
+            listOfMenuGroup[2].move(97, 158)
+        elif labelIndex == 1:
+            listOfMenuGroup[2].move(110, 158)
+        else:
+            listOfMenuGroup[2].move(130, 158)
+
+        # set titleLabelB if it exists
+        if menuData[1][0] != False:
+            listOfMenuGroup[2].setText(menuData[1][0])
+            listOfMenuGroup[2].setFont(
+                self.addFontWithPixel(10.5, 'Noto Sans KR', True))
+
+        # load menuLabel
+        menuText = ''
+        for i in range(1, len(menuData[1])):
+            menuText += menuData[1][i] + '\n'
+        listOfMenuGroup[4].setText(menuText)
+        listOfMenuGroup[4].setFont(
+            self.addFontWithPixel(13, 'Noto Sans KR Medium'))
+
+        # load priceLabel
+        listOfMenuGroup[5].setText(menuData[2])
+        listOfMenuGroup[5].setFont(
+            self.addFontWithPixel(14))
+
+    def loadmainGroup(self, tag):
+        cnt = 0
+        for reNum in range(3):
+            # check tag
+            for menu in self.reList[reNum]:
+                if tag in menu[3]:
+                    self.loadMenuGroup(
+                        reNum, self.reList[0][0], self.lomg_tup[cnt]
+                    )
+                    cnt += 1
+        return cnt
+
+    # Initialize the widget and save it as a tuple in the instance attribute
+    def initWidget(self):
+        self.listOfMenuGroup1 = (self.MenuGroup_1, self.titleLabel_1A, self.titleLabel_1B,
+                                 self.menuImage_1, self.menuLabel_1, self.priceLabel_1)
+        self.listOfMenuGroup2 = (self.MenuGroup_2, self.titleLabel_2A, self.titleLabel_2B,
+                                 self.menuImage_2, self.menuLabel_2, self.priceLabel_2)
+        self.listOfMenuGroup3 = (self.MenuGroup_3, self.titleLabel_3A, self.titleLabel_3B,
+                                 self.menuImage_3, self.menuLabel_3, self.priceLabel_3)
+        self.listOfMenuGroup4 = (self.MenuGroup_4, self.titleLabel_4A, self.titleLabel_4B,
+                                 self.menuImage_4, self.menuLabel_4, self.priceLabel_4)
+        self.listOfMenuGroup5 = (self.MenuGroup_5, self.titleLabel_5A, self.titleLabel_5B,
+                                 self.menuImage_5, self.menuLabel_5, self.priceLabel_5)
+        self.listOfMenuGroup6 = (self.MenuGroup_6, self.titleLabel_6A, self.titleLabel_6B,
+                                 self.menuImage_6, self.menuLabel_6, self.priceLabel_6)
+        self.listOfMenuGroup7 = (self.MenuGroup_7, self.titleLabel_7A, self.titleLabel_7B,
+                                 self.menuImage_7, self.menuLabel_7, self.priceLabel_7)
+        self.listOfMenuGroup8 = (self.MenuGroup_8, self.titleLabel_8A, self.titleLabel_8B,
+                                 self.menuImage_8, self.menuLabel_8, self.priceLabel_8)
+
+        self.lomg_tup = (self.listOfMenuGroup1, self.listOfMenuGroup2, self.listOfMenuGroup3, self.listOfMenuGroup4,
+                         self.listOfMenuGroup5, self.listOfMenuGroup6, self.listOfMenuGroup7, self.listOfMenuGroup8)
+
+    def hideAll(self):
+        for group in [self.MenuGroup_1, self.MenuGroup_2, self.MenuGroup_3, self.MenuGroup_4, self.MenuGroup_5, self.MenuGroup_6, self.MenuGroup_7, self.MenuGroup_8]:
+            group.hide()
+        self.refreshGif.hide()
+        self.refreshLabel.hide()
+
--- web/webScroll.py
+++ web/webScroll.py
@@ -21,7 +21,7 @@ def getReSoup(reNum):
 
 def getReList(reNum):
 
-    def heAppend(soup):
+    def heAppend(soup, tag):
         def imgURL(URL):
             if URL == '/html-repositories/images/custom/food/no-img.jpg':
                 return 'https://www.hanyang.ac.kr/html-repositories/images/custom/food/no-img.jpg'
@@ -44,16 +44,19 @@ def getReList(reNum):
         tempList.append(imgURL(soup.find('img').get('src')))
         tempList.append(menuToList(soup.find('img').get('alt')))
         tempList.append(soup.find('p', class_='price').get_text())
+        tempList.append(tag)
 
         return tempList
 
-    sortedSoup = getReSoup(reNum).find("div", class_='box tab-box-wrap tab-dth1 tab-lg', id='_foodView_WAR_foodportlet_tab_1') \
-        .find("div", class_='tab-pane active', id='messhall1').findAll('li', class_="span3")
-
+    foodBoxList = getReSoup(reNum).find("div", class_='box tab-box-wrap tab-dth1 tab-lg', id='_foodView_WAR_foodportlet_tab_1') \
+        .find("div", class_='tab-pane active', id='messhall1').find_all('div', class_='in-box')
     reList = []
 
-    for i in range(len(sortedSoup)):
-        reList.append(heAppend(sortedSoup[i]))
+    for box in foodBoxList:
+        tag = box.find('h4', class_='d-title2').get_text()
+        sortedSoup = box.findAll('li', class_="span3")
+        for i in range(len(sortedSoup)):
+            reList.append(heAppend(sortedSoup[i], tag))
 
     return reList
 
@@ -77,5 +80,6 @@ def getAllreList(process=4):
 
         print("[log.d] web loaded with time : {}".format(seq))
         return (finalReList, (re0Num, re1Num, re2Num))
+        # return ([[['https://www.hanyang.ac.kr/web/www/re2?p_p_id=foodView_WAR_foodportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_pos=1&p_p_col_count=2&_foodView_WAR_foodportlet_fileId=898008&_foodView_WAR_foodportlet_cmd=download&_foodView_WAR_foodportlet_sFoodDateYear=2022&_foodView_WAR_foodportlet_sFoodDateMonth=7&_foodView_WAR_foodportlet_action=view&_foodView_WAR_foodportlet_sFoodDateDay=11', ['[Pangeos]', '오므라이스', '코코넛새우가스', '데리야키볶음우동'], '4000₩']], [], []], (1, 0, 0))
     except Exception as e:
         return ('WEB ERROR', str(e))
