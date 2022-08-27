e0bed2cb2387cf7725501cd3fe55779e2e88ee60
laniel88 <ryanhb0430@gmail.com>
2022-08-27 09:23:33 +0900

    mod first

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from gui.loadWin import LoadWin
 
 if __name__ == "__main__":
 
-    multiprocessing.freeze_support()    # for windows exe (pyinstaller)
+    #multiprocessing.freeze_support()    # for windows exe (pyinstaller)
 
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
